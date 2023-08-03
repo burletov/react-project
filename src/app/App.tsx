e240cbea527b235377f0be9896938c059292bd18
Igor Burletov <burletov@bk.ru>
2023-08-03 18:01:09 +0300

    feature-slice.design

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import { Suspense } from 'react';
 import './styles/index.scss';
 import { Link, Route, Routes } from 'react-router-dom';
-import { MainPageAsync } from './pages/MainPage/MainPageAsync';
-import { AboutPageAsync } from './pages/AboutPage/AboutPageAsync';
-import { useTheme } from './theme/useTheme';
-import { classNames } from './helpers/classNames/classNames';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { MainPage } from 'pages/MainPage';
+import { AboutPage } from 'pages/AboutPage';
 
 const App = () => {
 	const { theme, toggleTheme } = useTheme();
@@ -16,8 +16,8 @@ const App = () => {
 				<Link to={'/about'}>О сайте</Link>
 				<Suspense fallback={<div>Loading...</div>}>
 					<Routes>
-						<Route path={'/'} element={<MainPageAsync />} />
-						<Route path={'/about'} element={<AboutPageAsync />} />
+						<Route path={'/'} element={<MainPage />} />
+						<Route path={'/about'} element={<AboutPage />} />
 					</Routes>
 				</Suspense>
 			</div>
